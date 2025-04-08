8313cf638066bf90d8eeaaa76223a4c0edc78aee
Jpfpp12 <118419073+Jpfpp12@users.noreply.github.com>
2025-04-08 19:27:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+  const output = document.getElementById("output");
+  const arr = [1, 2, 3, 4];
+
+  function evenOut() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const even = arr.filter(num => num % 2 === 0);
+        output.innerHTML = even.join(", ");
+        resolve(even); // Pass the data to next then()
+      }, 1000);
+    });
+  }
+
+  function multiplyBy2(numbers) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const multiplied = numbers.map(num => num * 2);
+        output.innerHTML = multiplied.join(", ");
+        resolve(); // Done!
+      }, 2000);
+    });
+  }
+
+  // Chaining the flow
+  evenOut()
+    .then(result => multiplyBy2(result));
